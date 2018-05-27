3dbe64269a22f060b76c72e2f1fc988826c79050
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2018-05-27 21:18:06 +0900

    Fix docker file for build dnn module.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 export WITH_CUDA=ON
 
-if [ $# -e 2 ]; then
+if [ $# -ne 0 ]; then
     export WITH_CUDA=OFF
 fi
 
 
-docker build --tag opencv:latest-env ./docker
+docker build --tag opencv:latest-env --build-arg WITH_CUDA="$WITH_CUDA" ./docker 
